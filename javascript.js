0fe3122967f9d0b5a7e26d00e06c32f98f0423ba
Shreyas <117995509+Philomath21@users.noreply.github.com>
2024-06-17 14:26:47 +0530

    create basic 16*16 grid of div using javascript

--- javascript.js
+++ javascript.js
@@ -1,1 +1,22 @@
+// get the container element
 let container = document.querySelector(".container");
+
+// creating 16*16 grid of div using two for loops
+for (let i=0; i<16; i++){
+    // create new div row element
+    const rowDiv = document.createElement("div");
+    // add a class "row-div"
+    rowDiv.classList.add("row-div");
+    // add that div element inside the container div
+    container.appendChild(rowDiv);
+    for(let j=0; j<16; j++) {
+        // create new square div element
+        const squareDiv = document.createElement("div");
+        // add a class "square-div"
+        squareDiv.classList.add("square-div");
+        // add that div element inside the row-div
+        rowDiv.appendChild(squareDiv);
+    }
+}
+
+
