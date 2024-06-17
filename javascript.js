573ece321d5f9060501eb69073e9928547d775c6
Shreyas <117995509+Philomath21@users.noreply.github.com>
2024-06-17 14:54:27 +0530

    add hover effect
    - add hover effect using event listener
    - clean up the css and javascript code a bit

--- javascript.js
+++ javascript.js
@@ -1,22 +1,25 @@
 // get the container element
 let container = document.querySelector(".container");
 
-// creating 16*16 grid of div using two for loops
+// create a 16*16 grid of div using two for loops
 for (let i=0; i<16; i++){
-    // create new div row element
     const rowDiv = document.createElement("div");
-    // add a class "row-div"
     rowDiv.classList.add("row-div");
-    // add that div element inside the container div
     container.appendChild(rowDiv);
     for(let j=0; j<16; j++) {
-        // create new square div element
         const squareDiv = document.createElement("div");
-        // add a class "square-div"
         squareDiv.classList.add("square-div");
-        // add that div element inside the row-div
+        // Set up a “hover” effect so that the grid divs change color 
+        // when your mouse passes over them, leaving a (pixelated) trail 
+        // through your grid like a pen would.
+        squareDiv.addEventListener("mouseover", () => {
+            squareDiv.style.backgroundColor = "black";
+        })
         rowDiv.appendChild(squareDiv);
     }
 }
+
+
+
 
 
