50f5b1784582765e348e171121c2c7f6233a6ebc
Shreyas <117995509+Philomath21@users.noreply.github.com>
2024-06-17 15:56:46 +0530

    add working size button

--- javascript.js
+++ javascript.js
@@ -1,23 +1,41 @@
 // get the container element
 let container = document.querySelector(".container");
 
-// create a 16*16 grid of div using two for loops
-for (let i=0; i<16; i++){
-    const rowDiv = document.createElement("div");
-    rowDiv.classList.add("row-div");
-    container.appendChild(rowDiv);
-    for(let j=0; j<16; j++) {
-        const squareDiv = document.createElement("div");
-        squareDiv.classList.add("square-div");
-        // Set up a “hover” effect so that the grid divs change color 
-        // when your mouse passes over them, leaving a (pixelated) trail 
-        // through your grid like a pen would.
-        squareDiv.addEventListener("mouseover", () => {
-            squareDiv.style.backgroundColor = "black";
-        })
-        rowDiv.appendChild(squareDiv);
+// Function to create board of custom sized pixels
+function createBoard (size) {
+    // create a size*size grid of div using two for loops
+    for (let i=0; i<size; i++){
+        const rowDiv = document.createElement("div");
+        rowDiv.classList.add("row-div");
+        container.appendChild(rowDiv);
+        for(let j=0; j<size; j++) {
+            const squareDiv = document.createElement("div");
+            squareDiv.classList.add("square-div");
+            // Set up a “hover” effect so that the grid divs change color 
+            // when your mouse passes over them, leaving a (pixelated) trail 
+            // through your grid like a pen would.
+            squareDiv.addEventListener("mouseover", () => {
+                squareDiv.style.backgroundColor = "black";
+            })
+            rowDiv.appendChild(squareDiv);
+        }
     }
 }
+
+// get size input from user, using size button
+function getSize() {
+    let x = Number(prompt("Please specify the number of squares per side for the new grid: "))
+    if (x == NaN | x < 1 | x >100) {
+        alert("Enter a valid number between 1 to 100")
+    } else {
+        createBoard(x);
+    }
+}
+const sizeButton = document.querySelector("#size");
+sizeButton.addEventListener("click",getSize);
+
+
+
 
 
 
